0ff101d59717b5a676fb7ca9df4f9082a7557314
OliverKahn <73127027+oliverkahn5305@users.noreply.github.com>
2022-01-31 13:43:14 +0200

    Add files via upload

---
+++ Harjutus1.1 Python.py
@@ -0,0 +1,15 @@
+# Ylessanne 1
+# Oliver Kahn
+# 31.01.2022
+
+print("\"Cabarnet Sauvignon\" või 'Cabarnet Sauvignon'?")
+ 
+print("  @..@")
+print(" (----)")
+print("( \__/ )")
+print('^^ "" ^^') 
+
+ #ruudu pindala
+ a = int(input("Sisesta ruudu külje pikkus: "))
+ s = a * a
+ print("Vastus: ruudu pindala on ", str(s)+"cm^2")
