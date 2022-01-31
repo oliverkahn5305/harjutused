c3ee049419d07a5138fd62cdfc695626788f50e0
OliverKahn <73127027+oliverkahn5305@users.noreply.github.com>
2022-01-31 14:00:12 +0200

    Add files via upload

---
+++ harjutus2.1 python.py
@@ -0,0 +1,15 @@
+# harjutus 2
+# Oliver Kahn
+# 31.01.2022
+
+#kolmnurga ümbermöödu arvutamine
+a = int(input("Sisestage külje pikkus "))
+b = int(input("Sisestage külje pikkus "))
+c = int(input("Sisestage külje pikkus "))
+P = a + b + c
+print("Vastus: kolmnurga ümbermõõt on ", str(P)+"cm^2")
+
+
+#toodete summa arvutamine
+Toote hind 36,75€
+Soodushind hetkel 40%
