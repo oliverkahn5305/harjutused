a5b52594a8b3cb61b6be88ab327480c337cac6b0
OliverKahn <73127027+oliverkahn5305@users.noreply.github.com>
2022-03-14 11:45:51 +0200

    Add files via upload

---
+++ kodutöö1.py
@@ -0,0 +1,26 @@
+#kodutöö kujund
+
+import turtle
+t = turtle.Turtle()
+aken = turtle.Screen
+k = turtle.Turtle()
+
+a = 50
+b = 90
+c = 25
+
+for i in range(4):
+    k.forward(c)
+    k.left(b)
+    k.forward(a)
+
+for d in range(4):
+    
+    
+
+
+    
+    
+    
+    
+aken.exitclickon()
