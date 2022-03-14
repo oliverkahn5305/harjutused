ae64883fc6e2850f44b7b6c4f980d4bc8796c767
OliverKahn <73127027+oliverkahn5305@users.noreply.github.com>
2022-03-14 11:16:42 +0200

    Add files via upload

---
+++ kujund1.py
@@ -0,0 +1,138 @@
+#Oliver Kahn
+#14.03.2022
+#1kujund
+
+#kolmnurk
+
+import turtle
+k = turtle.Turtle()
+ 
+for i in range(3):
+    k.forward(100)
+    k.left(120)
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+
+import turtle
+import random
+aken = turtle.Screen()
+aken.setup(300, 300)
+aken.title("harjutus 2")
+t = turtle.Turtle()
+angle = 0
+
+for i in range(5):
+    t.rt(angle)
+    t.forward(100)
+    angle=144
+    
+'''    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+import turtle
+
+aken = turtle.Screen()
+aken.setup(300, 300)
+aken.bgcolor('black')
+aken.title("Liigutame kilpkonna")
+
+turtle.color("orange")
+turtle.left(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+
+turtle.color("red")
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+
+turtle.color("blue")
+turtle.left(90)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+
+turtle.color("green")
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+
+turtle.color("blue")
+turtle.pensize(0.2)
+turtle.right(45)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+
+turtle.color("orange")
+turtle.pensize(0.2)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+
+turtle.color("green")
+turtle.pensize(0.2)
+turtle.left(75)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+
+turtle.color("red")
+turtle.pensize(0.2)
+turtle.forward(100)
+turtle.left(180)
+turtle.forward(100)
+'''
+
+
+
+
+
+
+
+
+
+turtle.exitonclick()
+
